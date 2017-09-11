754592dc3573d07b3fd2db531e1f51409c184028
Tavo <gariosp@unal.edu.co>
2017-09-11 15:55:06 -0300

    Specifications 2.0 cat-clicker for two independent cats

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -2,12 +2,17 @@
 
 function init(){
 	var catImage = $('.cat-image');
+	var elGatoName = ' elGato';
+	var theCatName = ' theCat';
+	$('#elGato p:first').append(elGatoName);
+	$('#theCat p:first').append(theCatName);
 	catImage.click(cat_clicked);
 }
 
 function cat_clicked(){
-	//console.log('Cat was indeed clicked!');
-	var $click_counter = $('.clicks-made');
+	var $parentId = $(this).parent().attr('id');
+	//console.log('Cat was indeed clicked! from: ' + $parentId);
+	var $click_counter = $('#'+ $parentId +' .clicks-made');
 	var click_count = parseInt($click_counter.text());
 	click_count++;
 	//console.log(click_count);
